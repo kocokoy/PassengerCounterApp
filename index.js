60c483bed8da8884f5f8b168307bafef4ab64262
Vann Erycko <vanneryckoconstantino@gmail.com>
2022-04-11 23:32:14 +0800

    button created

---
+++ index.js
@@ -0,0 +1,10 @@
+//document.getElementById("count-el").innerText = 5
+
+// intialize the count as 0
+//listen for clicks on the increment button
+//increment the count variable when the button is clicked
+//change the count-el in HTML to reflect the new count
+
+function increment(){
+    console.log("button")
+}
