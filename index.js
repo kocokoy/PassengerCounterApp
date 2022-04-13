cb3a43456aa57d30eadc53c5de2d63f165753e1c
Vann Erycko <vanneryckoconstantino@gmail.com>
2022-04-13 16:24:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,14 @@
 //listen for clicks on the increment button
 //increment the count variable when the button is clicked
 //change the count-el in HTML to reflect the new count
+//array
+let counteEl = document.getElementById("count-el")
+
+console.log(counteEl)
+
+let  count = 0;
+
+function increment() {
+   count += 1
+   console.log(count)
+}
